5192cf37f53b84c315ba11a5e5509261f75f0e00
sergioia-dev <sergioidarraga2110@gmail.com>
2026-09-13 10:23:16 -0500

    first functional version

--- derivations/project-tree-nvim/default.nix
+++ derivations/project-tree-nvim/default.nix
@@ -1,7 +1,16 @@
-{ vimUtils }:
+{
+  vimUtils,
+  fetchFromGitHub,
+}:
 vimUtils.buildVimPlugin {
   pname = "project-tree-nvim";
   version = "0.1.0";
-  src = ./../../plugin/project-tree-nvim;
+  src = fetchFromGitHub {
+
+    owner = "sergioia-dev";
+    repo = "project-tree.nvim";
+    rev = "main";
+    sha256 = "sha256-X3fHoLmf5Qd2HGdzcJT3qllOFmnQLMf+7SenK+cv56g=";
+  };
   meta.description = "Render project tree in a floating window using tree CLI";
 }
